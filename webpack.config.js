b9b5bb444c3bbc1f899f3a81592144b7572ff770
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-04-22 09:32:52 +0200

    create webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+
+        rules: [{
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+
+    devtool: 'source-map',
+
+
+    devServer: {
+        proxy: {
+            '/socket.io': {
+                target: 'http://localhost:3000',
+                ws: true
+            }
+        }
+    }
+
+};
